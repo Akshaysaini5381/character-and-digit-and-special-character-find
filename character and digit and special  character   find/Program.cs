2e635750ae0f722595a6a6858fcea8216f197653
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-05-03 01:03:45 +0530

    Add project files.

---
+++ character and digit and special  character   find/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace character_and_digit_and_special__character___find
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the value :-");
+            char data = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine(" This is  value ({0}) ",data);
+
+            if (data >= 65 && data <= 90 || data >= 97 && data <= 122)
+                Console.WriteLine("This is a Alphabat");
+            else if (data >= 48 && data <= 57)
+                Console.WriteLine("This is a Digit");
+            else
+                Console.WriteLine("Special character");
+            Console.ReadLine();
+        }
+       
+    }
+}
